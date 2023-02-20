fa88dc5bc562190d7cd06bf0d49bf6d9f2951659
Olena-Budnik <oobudnik@gmail.com>
2023-02-20 11:57:25 +0100

    add header,footer

---
+++ src/container/App/App.tsx
@@ -0,0 +1,13 @@
+import Footer from "container/Footer/Footer"
+import Header from "container/Header/Header"
+
+type Props = {}
+const App = (props: Props) => {
+  return (
+    <>
+       <Header/>
+       <Footer/>
+    </>
+  )
+}
+export default App
