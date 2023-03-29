c481d709adc6ffd1b5cc6404169bd0a8dee52209
Olena-Budnik <oobudnik@gmail.com>
2023-03-29 12:02:16 +0200

    change reviews style

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -10,8 +10,6 @@ import { Container } from '@mui/system'
 import { useState } from 'react'
 import './Revires.scss'
 
-
-
 type Props = {}
 
 type Review = {
@@ -78,50 +76,64 @@ const Reviews = (props: Props) => {
     }
     return (
         <>
-        <Container>
-        <div className='form-reviews'>
-            <Typography
-                variant="h4"
-                component="h2"
-                sx={{
-                    margin: '60px 0',
-                }}
-            >
-                Reviews
-            </Typography>
-            
-                {reviews.map((item, i) => (
-                    <Card variant="outlined" sx={{ margin: '20px 0' }} key={i}>
-                        <CardContent>
-                            <div>{item.name}:</div>
-                            <div>{item.text}</div>
-                        </CardContent>
-                    </Card>
-                ))}
-            </div>
-            <form onSubmit={onSend} className='form-reviews'>
-                <h3>Please leave a review</h3>
-                <div>
-                    <TextField
-                        label="Name"
-                        value={newReview.name}
-                        onChange={handleName}
-                    />
+            <Container>
+                <div className="form-reviews">
+                    <Typography
+                        variant="h4"
+                        component="h2"
+                        sx={{
+                            margin: '60px 0',
+                        }}
+                    >
+                        Reviews
+                    </Typography>
+
+                    {reviews.map((item, i) => (
+                        <Card
+                            variant="outlined"
+                            sx={{ margin: '20px 0' }}
+                            key={i}
+                        >
+                            <CardContent>
+                                <div>{item.name}:</div>
+                                <div>{item.text}</div>
+                            </CardContent>
+                        </Card>
+                    ))}
                 </div>
-                <br />
-                <div>
-                    <TextareaAutosize
-                        minRows={5}
-                        placeholder="Text"
-                        value={newReview.text}
-                        onChange={handleText}
-                        
-                    />
-                </div>
-                <Button variant="outlined" type="submit">
-                    Send
-                </Button>
-            </form>
+
+                <form onSubmit={onSend} className="form-reviews">
+                    <h2>Please leave a review</h2>
+                    <div className="name">
+                        <TextField
+                            label="Name"
+                            variant="standard"
+                            value={newReview.name}
+                            onChange={handleName}
+                        />
+                    </div>
+                    <br />
+                    <div className="text">
+          
+                        <TextField
+                            id="standard-multiline-static"
+                            label="Your massage:"
+                            multiline
+                            rows={4}
+                            defaultValue="Text"
+                            variant="standard"
+                            minRows={5}
+                            placeholder="Text"
+                            value={newReview.text}
+                            onChange={handleText}
+                        />
+                    </div>
+                    <div className="btn-send">
+                        <Button variant="outlined" type="submit">
+                            Send
+                        </Button>
+                    </div>
+                </form>
             </Container>
         </>
     )
